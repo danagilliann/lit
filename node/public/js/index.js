e17b40ccae649326fcb917bf017f938a7e55abd9
danagilliann <danalee96@gmail.com>
2016-04-03 04:29:30 -0400

    LIT FO SHO

--- node/public/js/index.js
+++ node/public/js/index.js
@@ -1,4 +1,6 @@
 var socket = io.connect();
-socket.on('connection', function(tweet){
-  $('#tweetd').append(tweet+'<br>');
+socket.on('stream', function(data){
+  if (data.lat !== undefined && data.lng !== undefined) {
+    $('body').append('<div>'+data.lat+data.lng+'</div>');
+  }
 });
--- node/routes/index.js
+++ node/routes/index.js
@@ -25,7 +25,44 @@ module.exports = function (io) {
   });
 
   io.on('connection', function(socket){
-    console.log('A user connected');
+
+    // twitter stream
+    var stream = T.stream("statuses/filter", { track: 'swarmapp', language: 'en'});
+    
+    stream.on('message', function(msg) {
+      if (msg.entities.urls[0] !== undefined && msg.entities.urls[0].display_url !== undefined) {
+        swarmUrl = msg.entities.urls[0].display_url;
+        swarmUrl = swarmUrl.split("/");
+        
+        if (swarmUrl[2] !== undefined) {
+          swarmUrl = swarmUrl[2]; // gets swarm id
+        }
+    
+        if (swarmUrl !== undefined) {
+          // swarm request
+          var foursquareRequest = "https://api.foursquare.com/v2/checkins/resolve?shortId=" + swarmUrl +"&oauth_token=1N5UGBL211K2VR5T2AFW0J0V4H5UWZPOUO4BFLPTSP1ENWPZ&v=20160402";
+    
+          request({
+            url: foursquareRequest,
+            json: true
+          }, function (error, response, body) {
+          
+            if (!error && response.statusCode === 200) {
+              console.log()
+              lat = body.response.checkin.venue.location.lat;
+              lng = body.response.checkin.venue.location.lng;
+              console.log(lat + ", " + lng); // Print the json response
+            }
+          });
+        }
+      }
+      socket.emit('stream', {lat: lat, lng: lng});
+    });
+    
+    stream.on('error', function(err) {
+      console.log(err);
+    });
+    
   });
 
   // io.sockets.on('connection', function(sockets) {
